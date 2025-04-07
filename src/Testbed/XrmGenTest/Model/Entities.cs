50e1ede278513038c0061d89968ad839291007ab
Reza <reza.niroomand@hotmail.com>
2025-04-08 00:57:12 +0200

    Add contactid attribute and update entity definitions
    Updated `ContactCreatePlugin.model.json` and `Entities.model.json` to include the `contactid` attribute in the `contact` entity. Enhanced metadata with new `MetadataId` values. Introduced a new partial class `Partner` in `Entities.cs` for the `account` entity, along with necessary using directives. Updated `Entities.yaml` to reflect the new structure for the `account` entity. Modified `XrmGenTest.csproj` to include `Entities.yaml` for generating `Entities.cs` and updated the `EnvironmentUrl`. These changes improve entity management and structure.

--- src/Testbed/XrmGenTest/Model/Entities.cs
+++ src/Testbed/XrmGenTest/Model/Entities.cs
@@ -4,7 +4,7 @@ using XrmTools.Meta.Attributes;
 
 [assembly: Entity("notification", AttributeNames = "createdon,eventid,notificationnumber,notificationid")]
 [assembly: Entity("account", AttributeNames = "name,accountnumber,telephone1,statecode,statuscode,primarycontactid,address1_city")]
-[assembly: Entity("contact", AttributeNames = "name,parentcontactid,annualincome,territorycode,accountid,accountidyominame")]
+[assembly: Entity("contact", AttributeNames = "name,parentcontactid,annualincome,territorycode,accountid,accountidyominame,contactid")]
 
 public class Test
 {
