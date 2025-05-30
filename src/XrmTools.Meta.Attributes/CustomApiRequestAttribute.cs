38148a7d291102d59b602d92f30c250da3239444
Reza <reza.niroomand@hotmail.com>
2025-05-30 19:44:39 +0200

    Revert XrmTools.Meta.Attribute to previous version.
    Ther is no use for the interfaces and the extension method and the changes in CustomApiRequestAttribute and CustomApiResponseAttribute, now that I generate Messages similar to Microsoft.

--- src/XrmTools.Meta.Attributes/CustomApiRequestAttribute.cs
+++ src/XrmTools.Meta.Attributes/CustomApiRequestAttribute.cs
@@ -4,27 +4,8 @@
 
     /// <summary>
     /// This attribute when used inside a CustomApi plugin class, marks the nested class that 
-    /// contains request parameters for the custom API. When used on a class that is not nested
-    /// and with the name of the custom API, it marks the class as a request class for calling the custom API.
+    /// contains request parameters for the custom API.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-    public class CustomApiRequestAttribute : Attribute 
-    {
-        public string CustomApiName { get; set; } = string.Empty;
-
-        /// <summary>
-        /// This constructor is used for nested classes that define request parameters for custom APIs.
-        /// </summary>
-        public CustomApiRequestAttribute() { }
-
-        /// <summary>
-        /// This constructor is used for request classes that act as OrganizationRequest for calling custom APIs.
-        /// </summary>
-        /// <param name="customApiName"></param>
-        public CustomApiRequestAttribute(string customApiName)
-        {
-            CustomApiName = customApiName;
-        }
-    }
-
+    public class CustomApiRequestAttribute : Attribute { }
 }
