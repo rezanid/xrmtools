808e7e8c03d7c10a9b2000448354e4866e87ce25
Reza <reza.niroomand@hotmail.com>
2025-05-30 19:55:19 +0200

    Update the testbed proj to test new custom refactoring providers.

--- src/Testbed/XrmGenTest/Client/Candidate1/CustomResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Xrm.Sdk;
-
-namespace XrmGenTest.Client;
-
-internal abstract class CustomResponse<T>
-    where T : CustomResponse<T>, new()
-{
-    public abstract T FromOrganizationResponse(OrganizationResponse response);
-}
