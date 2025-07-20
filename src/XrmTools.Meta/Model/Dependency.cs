f0efa40fe2a17d6c2ece5e3923e8eed07f468a55
Reza <reza.niroomand@hotmail.com>
2025-07-21 00:03:52 +0200

    Added more code files to Git from XrmTools.Meta

---
+++ src/XrmTools.Meta/Model/Dependency.cs
@@ -0,0 +1,13 @@
+﻿namespace XrmTools.Meta.Model;
+using System.Collections.Generic;
+
+public class Dependency
+{
+    public string? PropertyName { get; set; }
+    public string FullTypeName { get; set; } = string.Empty;
+    public string ShortTypeName { get; set; } = string.Empty;
+    public List<Dependency> Dependencies { get; set; } = [];
+    public bool IsProperty { get; set; }
+    public bool IsLocalVariableNeeded { get; set; }
+    public string? ProvidedByBaseProperty { get; set; }
+}
---
+++ src/XrmTools.Meta/WebApi/Entities/SdkMessageFilter.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+namespace XrmTools.WebApi.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Text.Json.Serialization;
+using XrmTools.WebApi.Entities.Attributes;
+
+[EntityMetadata("sdkmessagefilter", "sdkmessagefilters")]
+public class SdkMessageFilter : Component<SdkMessageFilter>
+{
+    [JsonProperty("sdkmessagefilterid"), JsonPropertyName("sdkmessagefilterid")]
+    public override Guid? Id { get; set; }
+
+    [JsonProperty("primaryobjecttypecode"), JsonPropertyName("primaryobjecttypecode")]
+    public string? PrimaryObjectTypeCode { get; set; }
+
+    [JsonProperty("iscustomprocessingstepallowed"), JsonPropertyName("iscustomprocessingstepallowed")]
+    public bool? IsCustomProcessingStepAllowed { get; set; }
+
+    [JsonProperty("sdkmessageid"), JsonPropertyName("sdkmessageid")]
+    public Guid? SdkMessageId { get; set; }
+}
+#nullable restore
