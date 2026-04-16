d9f4f55570254a8d3c3329175f39a2605f71d84c
Reza <reza.niroomand@hotmail.com>
2026-04-16 17:13:28 +0200

    feat(template): add Power Platform plugin item template
    Introduced a new Visual Studio item template for Power Platform plugins. Includes a sample plugin class with registration attributes and dependency injection, assembly metadata, project configuration, template icon, and template definition for streamlined plugin development.

---
+++ src/Templates/XrmTools.PluginItemTemplate/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XrmTools.PluginItemTemplate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Reza Niroomand")]
+[assembly: AssemblyProduct("XrmTools.PluginItemTemplate")]
+[assembly: AssemblyCopyright("Copyright © Reza Niroomand 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d9cb135c-4c00-4c8a-b5df-cc4c663a5fb4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
