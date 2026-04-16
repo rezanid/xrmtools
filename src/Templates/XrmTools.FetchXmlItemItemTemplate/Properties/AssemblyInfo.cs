cfbc2440143ce3304c26aff38f783a2110db79cf
Reza <reza.niroomand@hotmail.com>
2026-04-16 17:12:19 +0200

    feat(template): add FetchXML item template project
    Added a new Visual Studio item template for Power Platform FetchXML queries. Includes project file, assembly info, sample FetchXML file, template definition, and icon. The template provides a basic FetchXML query and supports code generation.

---
+++ src/Templates/XrmTools.FetchXmlItemItemTemplate/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XrmTools.FetchXmlItemTemplate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Reza Niroomand")]
+[assembly: AssemblyProduct("XrmTools.FetchXmlItemTemplate")]
+[assembly: AssemblyCopyright("Copyright © Reza Niroomand 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6AC5059A-920F-4AE7-BAB4-851B96751AE0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
