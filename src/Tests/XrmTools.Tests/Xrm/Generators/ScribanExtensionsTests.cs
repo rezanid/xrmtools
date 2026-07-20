e908d13b7ab320343f26701f3942df7ceaea3f51
Reza <reza.niroomand@hotmail.com>
2026-07-20 16:20:29 +0200

    Ensure unique plugin step type names; add tests
    Added StepTypeDiscriminator to ScribanExtensions for unique plugin step type names when messages/stages overlap. Updated Scriban templates to use this logic for class/property naming. Added CSharpXrmMetaParserPrefixTests and PluginStepsTemplateTests for coverage. Enhanced ScribanExtensionsTests for discriminator logic.

--- src/Tests/XrmTools.Tests/Xrm/Generators/ScribanExtensionsTests.cs
+++ src/Tests/XrmTools.Tests/Xrm/Generators/ScribanExtensionsTests.cs
@@ -1,6 +1,11 @@
 ﻿namespace XrmTools.Tests.Xrm.Generators;
 
 using FluentAssertions;
+using System.Collections.Generic;
+using Scriban;
+using Scriban.Runtime;
+using XrmTools.Meta.Attributes;
+using XrmTools.Meta.Model.Configuration;
 using XrmTools.WebApi.Types;
 using XrmTools.Xrm.Generators;
 using Xunit;
@@ -68,5 +73,91 @@ public class ScribanExtensionsTests
         };
 
         ScribanExtensions.GetLabel(label, 1033).Should().BeNull();
+    }
+
+    private static PluginStepConfig Step(string message, Stages? stage) =>
+        new() { MessageName = message, Stage = stage };
+
+    [Fact]
+    public void StepTypeDiscriminator_ReturnsEmpty_ForSingleStep()
+    {
+        var step = Step("Update", Stages.PostOperation);
+        var steps = new List<PluginStepConfig> { step };
+
+        ScribanExtensions.StepTypeDiscriminator(steps, step).Should().BeEmpty();
+    }
+
+    [Fact]
+    public void StepTypeDiscriminator_ReturnsEmpty_WhenMessagesAreDistinct()
+    {
+        var create = Step("Create", Stages.PostOperation);
+        var update = Step("Update", Stages.PostOperation);
+        var steps = new List<PluginStepConfig> { create, update };
+
+        ScribanExtensions.StepTypeDiscriminator(steps, create).Should().BeEmpty();
+        ScribanExtensions.StepTypeDiscriminator(steps, update).Should().BeEmpty();
+    }
+
+    [Fact]
+    public void StepTypeDiscriminator_UsesStageName_WhenMessageSharedWithDifferentStages()
+    {
+        var pre = Step("Update", Stages.PreOperation);
+        var post = Step("Update", Stages.PostOperation);
+        var steps = new List<PluginStepConfig> { pre, post };
+
+        ScribanExtensions.StepTypeDiscriminator(steps, pre).Should().Be("PreOperation");
+        ScribanExtensions.StepTypeDiscriminator(steps, post).Should().Be("PostOperation");
+    }
+
+    [Fact]
+    public void StepTypeDiscriminator_AppendsOrdinal_WhenMessageAndStageShared()
+    {
+        var first = Step("Update", Stages.PostOperation);
+        var second = Step("Update", Stages.PostOperation);
+        var steps = new List<PluginStepConfig> { first, second };
+
+        ScribanExtensions.StepTypeDiscriminator(steps, first).Should().Be("PostOperation1");
+        ScribanExtensions.StepTypeDiscriminator(steps, second).Should().Be("PostOperation2");
+    }
+
+    [Fact]
+    public void StepTypeDiscriminator_OnlyDisambiguatesCollidingMessages()
+    {
+        var createPost = Step("Create", Stages.PostOperation);
+        var updatePre = Step("Update", Stages.PreOperation);
+        var updatePost = Step("Update", Stages.PostOperation);
+        var steps = new List<PluginStepConfig> { createPost, updatePre, updatePost };
+
+        ScribanExtensions.StepTypeDiscriminator(steps, createPost).Should().BeEmpty();
+        ScribanExtensions.StepTypeDiscriminator(steps, updatePre).Should().Be("PreOperation");
+        ScribanExtensions.StepTypeDiscriminator(steps, updatePost).Should().Be("PostOperation");
+    }
+
+    [Fact]
+    public void StepTypeDiscriminator_ReturnsEmpty_WhenArgumentsAreNull()
+    {
+        ScribanExtensions.StepTypeDiscriminator(null, Step("Update", Stages.PostOperation)).Should().BeEmpty();
+        ScribanExtensions.StepTypeDiscriminator(new List<PluginStepConfig>(), null).Should().BeEmpty();
+    }
+
+    [Fact]
+    public void StepTypeDiscriminator_IsCallableFromScriban_ProducingUniqueTypeNames()
+    {
+        var pre = Step("Update", Stages.PreOperation);
+        var post = Step("Update", Stages.PostOperation);
+        var steps = new List<PluginStepConfig> { pre, post };
+
+        var scriptObject = new ScriptObject();
+        scriptObject.Import(typeof(ScribanExtensions));
+        scriptObject.Add("steps", steps);
+
+        var template = Template.Parse(
+            "{{~for step in steps~}}{{steps | step_type_discriminator step}}Update|{{~end~}}");
+        template.HasErrors.Should().BeFalse(string.Join("\n", template.Messages));
+
+        var context = new TemplateContext();
+        context.PushGlobal(scriptObject);
+
+        template.Render(context).Should().Be("PreOperationUpdate|PostOperationUpdate|");
     }
 }
